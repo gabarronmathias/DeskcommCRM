6e37478444e4446e2a9b25b9301a1e0e8152783a
gabarronmathias <diretoria@gabarronmathias.com>
2026-08-22 10:41:45 -0300

    feat(login): assemble Gabarron & Mathias login logo

---
+++ lib/branding/login-logo.ts
@@ -0,0 +1,7 @@
+import { LOGIN_LOGO_PART_1 } from "./login-logo-part1";
+import { LOGIN_LOGO_PART_2 } from "./login-logo-part2";
+import { LOGIN_LOGO_PART_3 } from "./login-logo-part3";
+import { LOGIN_LOGO_PART_4 } from "./login-logo-part4";
+
+export const GM_LOGIN_LOGO_DATA_URI =
+  `data:image/webp;base64,${LOGIN_LOGO_PART_1}${LOGIN_LOGO_PART_2}${LOGIN_LOGO_PART_3}${LOGIN_LOGO_PART_4}`;
